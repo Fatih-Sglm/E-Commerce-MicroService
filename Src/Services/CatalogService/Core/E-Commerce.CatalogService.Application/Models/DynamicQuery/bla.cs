37715546bbace30669055cc6fe1ec1f7e5bd4fd2
Fatih SAĞLAM <92210948+Fatih-Sglm@users.noreply.github.com>
2023-03-11 22:04:29 +0300

    Code Clean up

--- Src/Services/CatalogService/Core/E-Commerce.CatalogService.Application/Models/DynamicQuery/bla.cs
+++ Src/Services/CatalogService/Core/E-Commerce.CatalogService.Application/Models/DynamicQuery/bla.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace E_Commerce.CatalogService.Application.Models.DynamicQuery
+﻿namespace E_Commerce.CatalogService.Application.Models.DynamicQuery
 {
     public class bla
     {
--- Src/Services/CatalogService/Infrastructure/E-Commerce.CatalogService.Infrastructure/Services/Storage/Storage.cs
+++ Src/Services/CatalogService/Infrastructure/E-Commerce.CatalogService.Infrastructure/Services/Storage/Storage.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace E_Commerce.CatalogService.Infrastructure.Services.Storage
+﻿namespace E_Commerce.CatalogService.Infrastructure.Services.Storage
 {
     public class Storage
     {
